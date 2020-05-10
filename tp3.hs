8626f61dea57864aa90a84bc32da04e1738db715
Francipolla <fcipolla@est.frba.utn.edu.ar>
2020-05-10 19:34:37 -0300

    corregido

--- tp3.hs
+++ tp3.hs
@@ -13,12 +13,12 @@ propiedadesCompradas :: [Propiedad],
 accionesARealizar    :: [Accion]
 } deriving(Show)
 
-type Accion = (String, Participante -> Participante)
+type Accion = Participante -> Participante
 
 carolina :: Participante
-carolina = UnParticipante "Carolina" 500 "Accionista"        [] [("Pasar por el banco", pasarPorElBanco) , ("Pagar a accionistas" , pagarAAccionistas)]
+carolina = UnParticipante "Carolina" 500 "Accionista"        [] [ pasarPorElBanco, pagarAAccionistas]
 manuel   :: Participante 
-manuel   = UnParticipante "Manuel"   500 "Oferente singular" [] [("Pasar por el banco" , pasarPorElBanco)  , ("Enojarse" , enojarse)]
+manuel   = UnParticipante "Manuel"   500 "Oferente singular" [] [ pasarPorElBanco, enojarse]
 
 ------------------ Propiedades --------------------
 type Propiedad = (String, Int)
@@ -43,57 +43,52 @@ centralPerk             = ("Central Perk", 90)
 
 ------------------ Acciones -----------------------
 
-pasarPorElBanco :: Participante -> Participante
+pasarPorElBanco :: Accion
 pasarPorElBanco unJugador = ((cambiarTactica "Comprador Compulsivo").(agregarDinero 40)) unJugador
 
-cambiarTactica  :: String -> Participante -> Participante
+cambiarTactica  :: String -> Accion
 cambiarTactica nuevaTactica unJugador = unJugador { tacticaDeJuego = nuevaTactica}
 
-agregarDinero   :: Int -> Participante -> Participante
+agregarDinero   :: Int -> Accion
 agregarDinero dinero unJugador = unJugador { cantidadDeDinero = (cantidadDeDinero unJugador) + dinero}
 
-pagarAAccionistas :: Participante -> Participante
+pagarAAccionistas :: Accion
 pagarAAccionistas unJugador | (tacticaDeJuego unJugador) == "Accionista" = agregarDinero 200 unJugador | otherwise = restarDinero 100 unJugador
 
-restarDinero   :: Int -> Participante -> Participante
+restarDinero   :: Int -> Accion
 restarDinero dinero unJugador = unJugador { cantidadDeDinero = (cantidadDeDinero unJugador) - dinero}
 
-enojarse :: Participante -> Participante
-enojarse unJugador =  ((agregarDinero 50).(gritar (++"AHHHH"))) unJugador
+enojarse :: Accion
+enojarse unJugador =  ((agregarDinero 50).gritar) unJugador
 
-gritar :: (String -> String) -> Participante -> Participante 
-gritar grito unJugador = unJugador { nombre = grito (nombre unJugador)}
+gritar :: Accion
+gritar unJugador = unJugador { nombre = ("AHHHH"++)(nombre unJugador)}
 
-agregarAccion :: Accion -> Participante -> Participante
+agregarAccion :: Accion -> Accion
 agregarAccion accion unJugador = unJugador {accionesARealizar = accion : (accionesARealizar unJugador)}
 
 subastar :: Participante -> Propiedad -> Participante
-subastar unJugador propiedad | tacticaAceptable unJugador = unJugador {propiedadesCompradas = propiedad : (propiedadesCompradas unJugador)}
+subastar unJugador propiedad | tacticaAceptable unJugador = (restarDinero (precioPropiedad propiedad)) (unJugador {propiedadesCompradas = propiedad : (propiedadesCompradas unJugador)}) 
                              | otherwise = unJugador
 
 tacticaAceptable   :: Participante -> Bool
 tacticaAceptable unJugador = (tacticaDeJuego unJugador) == "Oferente singular" || (tacticaDeJuego unJugador) == "Accionista"
 
-cobrarAlquiler     :: Participante -> Participante
-cobrarAlquiler unJugador = unJugador {cantidadDeDinero = (cantidadDeDinero unJugador) + (10*((length.propiedadesBaratas) unJugador)) + (20*((length.propiedadesCaras) unJugador))}
+cobrarAlquiler     :: Accion
+cobrarAlquiler unJugador = agregarDinero ((10*((length.propiedadesBaratas) unJugador)) + (20*((length.propiedadesCaras) unJugador))) unJugador
+
+esBarata :: Int -> Bool
+esBarata = (<150)
 
 propiedadesBaratas :: Participante -> [Int]
-propiedadesBaratas unJugador = ((filter (<150)).listaDePrecios) unJugador
+propiedadesBaratas unJugador = ((filter esBarata).listaDePrecios) unJugador
 
 propiedadesCaras   :: Participante -> [Int]
-propiedadesCaras   unJugador = ((filter (>=150)).listaDePrecios) unJugador
+propiedadesCaras   unJugador = ((filter (not.esBarata)).listaDePrecios) unJugador
 
 listaDePrecios :: Participante -> [Int]
-listaDePrecios unJugador = map precioPropiedad (propiedadesCompradas unJugador)
+listaDePrecios unJugador =((map precioPropiedad).propiedadesCompradas) unJugador
 
 precioPropiedad :: Propiedad -> Int
 precioPropiedad (a,b) = b
 
-realizarAcciones :: Participante -> [Participante]
-realizarAcciones unJugador = map (\unaAccion -> unaAccion unJugador) (listaDeAcciones unJugador)
-
-listaDeAcciones :: Participante -> [Participante -> Participante]
-listaDeAcciones unJugador = map accionDeJugador (accionesARealizar unJugador)
-
-accionDeJugador :: Accion -> (Participante -> Participante)
-accionDeJugador (a,b) = b
