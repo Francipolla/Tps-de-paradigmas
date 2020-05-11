e09e484c72594a8f9f3ae5435c3e38a8bf06e002
Francipolla <fcipolla@est.frba.utn.edu.ar>
2020-05-10 22:50:54 -0300

    Corregi2

--- tp.hs
+++ tp.hs
@@ -1,3 +1,5 @@
+import Text.Show.Functions
+
 main :: IO ()
 main = return ()
 
@@ -31,35 +33,72 @@ reverse ::
 	"!!saneuB"
 --}
 
-convertirFecha dia mes anio = Show dia mes anio
-diaSencillo = even.(length.convertirFecha)
+convertirDia    ::  Show a => a -> String
+convertirDia  dia = show dia ++ "/"
 
+convertirMes    ::  Show a => a -> String
+convertirMes  mes = show mes ++ "/"
+
+convertirAnio   ::  Show a => a -> String
+convertirAnio  anio = show anio
+
+convertirFecha  :: Int -> Int -> Int -> String
+convertirFecha dia mes anio = (convertirDia dia) ++ (convertirMes mes) ++ (convertirAnio anio)
+
+diaSencillo     :: Int -> Int -> Int -> Bool 
+diaSencillo dia mes anio = even (length (convertirFecha dia mes anio))
+
+entregaSencilla :: Int -> Int -> Int -> Bool 
 entregaSencilla dia mes anio = diaSencillo dia mes anio
 
+inicialDeNombre :: String -> Char
+inicialDeNombre nombreDeProducto = head nombreDeProducto
 
-inicialDeNombre nombreDeProducto = head nombreDeProducto 
+largoDeNombreDeProducto :: String -> Int
 largoDeNombreDeProducto nombreDeProducto = length nombreDeProducto
+
+presenciaDeXEnNombre    :: String -> Bool
 presenciaDeXEnNombre nombreDeProducto = elem 'x' nombreDeProducto
+
+presenciaDeZEnNombre    :: String -> Bool
 presenciaDeZEnNombre nombreDeProducto = elem 'z' nombreDeProducto
+
+compararConVocales      :: Char -> Bool
 compararConVocales inicial = inicial == 'A' || inicial == 'E' || inicial == 'I' || inicial == 'O' || inicial == 'U'
 
-sacarLetras nombreDeProducto = ((length nombreDeProducto)<10)-10
+sacarLetras :: String -> String
+sacarLetras nombre | (length nombre) > 10 = take 10 nombre
+                   | otherwise =  nombre
+                   
+invertirNombre    :: String -> String
 invertirNombre nombreDeProducto = reverse nombreDeProducto
 
+productoCodiciado :: String -> Bool
+productoCodiciado nombreDeProducto = (largoDeNombreDeProducto nombreDeProducto) > 10
 
-productoDeLujo nombreDeProducto = presenciaDeXEnNombre nombreDeProducto || presenciaDeZEnNombre nombreDeProducto
-productoCodiciado = largoDeNombreDeProducto > 10
-productoCorriente =  compararConVocales.inicialDeNombre
+productoCorriente :: String -> Bool
+productoCorriente nombreDeProducto =  (compararConVocales.inicialDeNombre)nombreDeProducto
+
+productoXL :: String -> String
 productoXL nombreDeProducto = nombreDeProducto++"XL"
 
-
+descodiciarProducto :: String -> String
 descodiciarProducto nombreDeProducto = sacarLetras nombreDeProducto
 
-versionBarata = (reverse.descodiciarProducto)
+versionBarata :: String -> String
+versionBarata nombreDeProducto = (reverse.descodiciarProducto) nombreDeProducto
 
-productoDeElite nombreDeProducto = (productoDeLujo nombreDeProducto) && (productoCodiciado nombreDeProducto) && not(productoCorriente)
+productoDeElite :: String -> Bool
+productoDeElite nombreDeProducto = (productoDeLujo nombreDeProducto) && (productoCodiciado nombreDeProducto) && (not(productoCorriente nombreDeProducto))
 
+aplicarCostoDeEnvio :: Num a => a -> a -> a
 aplicarCostoDeEnvio costoDeEnvio precio = precio+costoDeEnvio
+
+aplicarDescuento :: Num a => a -> a -> a
 aplicarDescuento descuento precio = precio*descuento
 
-precioTotal precioUnitario cantidad descuento costoDeEnvio = aplicarCostoDeEnvio(costoDeEnvio (aplicarDescuento descuento precioUnitario)*cantidad)
+precioTotal ::  Num a => a -> a -> a -> a -> a
+precioTotal precioUnitario cantidad descuento costoDeEnvio = ((aplicarCostoDeEnvio costoDeEnvio).(aplicarDescuento descuento)) (precioUnitario*cantidad)
+
+productoDeLujo     :: String -> Bool
+productoDeLujo nombreDeProducto = (presenciaDeXEnNombre nombreDeProducto) || (presenciaDeZEnNombre nombreDeProducto)
